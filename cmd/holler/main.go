56e5dee9022b621d8baf276f050e1840a8a437b7
Jeff Malnick <malnick@gmail.com>
2017-03-05 10:37:25 -0800

    gitignore and build pipeline changes

--- cmd/holler/main.go
+++ cmd/holler/main.go
@@ -1,8 +1,27 @@
 package main
 
-import "github.com/malnick/holler"
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/malnick/holler"
+)
+
+var (
+	VERSION  = "unset"
+	REVISION = "unset"
+
+	versionFlag = flag.Bool("version", false, "Print holler version")
+)
 
 func main() {
+	flag.Parse()
+	if *versionFlag {
+		fmt.Printf("Holler Proxy\n    Version: %s\n    Revision: %s\n", VERSION, REVISION)
+		os.Exit(0)
+	}
+
 	myHoller, err := holler.New()
 	if err != nil {
 		panic(err)
